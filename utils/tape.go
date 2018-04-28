bb7a7d5132de4cc0c9414aeb7655a0d9b9fd86fc
Ben McClelland <ben.mcclelland@versity.com>
2018-04-28 10:30:58 -0700

    sort tape devs

--- utils/tape.go
+++ utils/tape.go
@@ -5,10 +5,12 @@ import (
 	"io/ioutil"
 	"path"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
 var devRxp = regexp.MustCompile(`^(st\d*)$`)
+var devNumRxp = regexp.MustCompile(`^st(\d*)$`)
 
 var statfiles = []string{
 	"in_flight",
@@ -27,17 +29,22 @@ const DEVPATH = "/sys/class/scsi_tape"
 
 type TapeStats map[string]int64
 
+type byValue []string
+
 func FindDevices() ([]string, error) {
 	var devs []string
 	dirents, err := ioutil.ReadDir(DEVPATH)
 	if err != nil {
 		return devs, err
 	}
+
 	for _, dirent := range dirents {
 		if devRxp.MatchString(dirent.Name()) {
 			devs = append(devs, dirent.Name())
 		}
 	}
+
+	sort.Sort(byValue(devs))
 	return devs, nil
 }
 
@@ -75,4 +82,20 @@ func PrintStats(s TapeStats) {
 	for k, v := range s {
 		fmt.Println(k, v)
 	}
+}
+
+func (s byValue) Len() int {
+	return len(s)
+}
+func (s byValue) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byValue) Less(i, j int) bool {
+	imatch := devNumRxp.FindStringSubmatch(s[i])
+	ival, _ := strconv.Atoi(imatch[1])
+
+	jmatch := devNumRxp.FindStringSubmatch(s[j])
+	jval, _ := strconv.Atoi(jmatch[1])
+
+	return ival < jval
 }
